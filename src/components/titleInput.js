7539dbc3cc86ac26b2327710625fabcd6526c480
Michael Bromley <michael@michaelbromley.co.uk>
2015-09-18 21:07:02 +0200

    Get titleInput component working

--- src/components/titleInput.js
+++ src/components/titleInput.js
@@ -4,14 +4,33 @@ class TitleInput extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            focus: false
+        };
     }
 
+    focus = () => {
+        this.setState({
+            focus: true
+        });
+        setTimeout(() => React.findDOMNode(this.refs.input).focus(), 0);
+    };
+
+    blur = () => {
+        this.setState({
+            focus: false
+        });
+    };
+
     render() {
+        var outerClass = 'title-input ' + (this.state.focus ? 'focus' : '');
         return (
-            <div className="titleInput">
+            <div className={outerClass} tabIndex="0" onClick={this.focus} onFocus={this.focus}>
                 <h2>{this.props.title}</h2>
                 <div className="input">
-                    <input value={this.props.title} onChange={this.props.onChange.bind(this)} />
+                    <input  ref="input" value={this.props.title}
+                            onChange={this.props.onChange.bind(this)}
+                            onBlur={this.blur} />
                 </div>
             </div>
         );
